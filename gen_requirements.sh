cb802fd8fb61a1e844b91bb61e2d48ee76378d79
Mika Lindqvist <postmaster@raasu.org>
2022-05-21 13:04:56 +0300

    Fix generating requirements.

--- gen_requirements.sh
+++ gen_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pipenv lock -r | cut -d ' ' -f 1 - > requirements.txt
-pipenv lock -r --dev | cut -d ' ' -f 1 - > requirements-dev.txt
+pipenv lock -r | grep -v '^\-i ' | cut -d ';' -f 1 - | cut -d ' ' -f 1 - > requirements.txt
+pipenv lock -r --dev | grep -v '^\-i ' | cut -d ';' -f 1 - | cut -d ' ' -f 1 - > requirements-dev.txt
